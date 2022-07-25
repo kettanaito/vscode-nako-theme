1ed00fa1ac0190bb74582c97d6fe7eae226d3c12
Artem Zakharchenko <kettanaito@gmail.com>
2022-07-25 17:39:06 +0200

    feat: use a new theme

--- demos/react.js
+++ demos/react.js
@@ -1,11 +1,41 @@
-import { Component } from "React"
+import { forwardRef } from "react";
+import { RunButton } from "../../common/RunButton";
+import { SandpackStack } from "../../common/Stack";
+import { useActiveCode } from "../../hooks/useActiveCode";
+import { useSandpack } from "../../hooks/useSandpack";
+import { CodeEditor } from "../CodeEditor";
+import type { Decorators } from "../CodeEditor/CodeMirror";
+import { FileTabs } from "../FileTabs";
 
-class Wow extends Component {
-  render() {
-    return (
-      <div className="wow">
-        <input type="number" />
-      </div>
-    )
-  }
+export interface CodeViewerProps {
+  showTabs?: boolean;
+  showLineNumbers?: boolean;
+  decorators?: Decorators;
+  code?: string;
 }
+
+export const SandpackCodeViewer = forwardRef<HTMLDivElement, CodeViewerProps>(
+  ({ showTabs, showLineNumbers, decorators, code: propCode }, ref) => {
+    const { sandpack } = useSandpack();
+    const { code } = useActiveCode();
+
+    const shouldShowTabs = showTabs ?? sandpack.openPaths.length > 1;
+
+    return (
+      <SandpackStack>
+        {shouldShowTabs ? <FileTabs /> : null}
+
+        <CodeEditor
+          ref={ref}
+          code={propCode ?? code}
+          decorators={decorators}
+          filePath={sandpack.activePath}
+          showLineNumbers={showLineNumbers}
+          readOnly
+        />
+
+        {sandpack.status === "idle" ? <RunButton /> : null}
+      </SandpackStack>
+    );
+  }
+);
