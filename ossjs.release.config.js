ee3ef75c17299de68fbc778c27e29ee6e10eb4db
Artem Zakharchenko <kettanaito@gmail.com>
2022-07-31 12:33:14 +0200

    chore: use "VSCE_PAT" so vsce grabs it

--- ossjs.release.config.js
+++ ossjs.release.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   script:
-    "vsce publish -p $VSCE_TOKEN --no-git-tag-version --no-update-package-json $RELEASE_VERSION",
+    "vsce publish --no-git-tag-version --no-update-package-json $RELEASE_VERSION",
 }
